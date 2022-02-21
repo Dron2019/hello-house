bbae21a7ebb2dfe1a5f0343a1207eab0410c51b3
Aleksandra Pedan <sashka28465@gmail.com>
2022-02-21 17:11:32 +0200

    video btn

---
+++ src/assets/scripts/gulp-modules/header.js
@@ -0,0 +1,68 @@
+function menuOpen(menu) {
+  menu.classList.add('menu__active');
+  const createAnimation = (links, translateY = 0, delay = 0) => {
+    links.forEach((link, i) => {
+      // eslint-disable-next-line no-undef
+      gsap.from(link, {
+        delay: delay + i / 10,
+        y: translateY,
+        opacity: 0,
+      });
+    });
+  };
+
+  const links1 = menu.querySelectorAll('[data-animation1]');
+  const links2 = menu.querySelectorAll('[data-animation2]');
+  const links3 = menu.querySelectorAll('[data-animation3]');
+  createAnimation(links1, 100, 0.5);
+  createAnimation(links2, 100, 0.2);
+  createAnimation(links3, 100, 1.1);
+}
+
+function menuClose(menu) {
+  menu.classList.remove('menu__active');
+}
+
+function menuInit() {
+  const menu = document.querySelector('[data-menu]');
+  document.querySelector('[data-open-menu]').addEventListener('click', () => menuOpen(menu));
+  document.querySelector('[data-close-menu]').addEventListener('click', () => menuClose(menu));
+}
+
+function init() {
+  const unSelectHandler = container => {
+    const elem = container.querySelector('.select-items');
+    if (!elem.classList.contains('select-hide')) {
+      container.classList.remove('select-arrow-active');
+      elem.classList.add('select-hide');
+    }
+    window.removeEventListener('click', unSelectHandler);
+  };
+  // const selectHandler = event => {
+  //   event.stopPropagation();
+  //   const container = event.target.closest('[data-lang]');
+  //   container.classList.add('select-arrow-active');
+  //   container.querySelector('.select-items').classList.remove('select-hide');
+  //   window.addEventListener('click', unSelectHandler.bind(null, container));
+  // };
+  // document.querySelector('[data-lang="mobile"]').addEventListener('click', selectHandler);
+  // document.querySelector('[data-lang="desktop"]').addEventListener('click', selectHandler);
+
+  menuInit();
+}
+
+window.addEventListener('DOMContentLoaded', init);
+
+// button hover start
+const btn = document.querySelectorAll('.js-button');
+btn.forEach(el =>
+  el.addEventListener('mousemove', e => {
+    const x = e.pageX - el.offsetLeft;
+    const y = e.pageY - el.offsetTop;
+
+    el.style.setProperty('--x', x + 'px');
+    el.style.setProperty('--y', y + 'px');
+  }),
+);
+
+// button hover end
---
+++ src/assets/scripts/gulp-modules/home.js
@@ -0,0 +1,144 @@
+const btn1 = document.querySelectorAll('.js-hover');
+
+btn1.forEach(el =>
+  el.addEventListener('mouseenter', () => {
+    el.previousElementSibling.style.opacity = '1';
+  }),
+);
+btn1.forEach(el =>
+  el.addEventListener('mouseleave', () => {
+    el.previousElementSibling.style.opacity = '0';
+  }),
+);
+
+// video start
+const videoHover = document.querySelector('.js-video');
+
+videoHover.addEventListener('mouseenter', () => {
+  document.querySelector('.texture').style.opacity = '0';
+});
+videoHover.addEventListener('mouseleave', () => {
+  document.querySelector('.texture').style.opacity = '1';
+});
+
+// video end
+
+const swiper = new Swiper('.benefits-swiper', {
+  slidesPerView: 2.3,
+  // centeredSlides: true,
+  spaceBetween: 40,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.benefits-next',
+    prevEl: '.benefits-prev',
+  },
+});
+
+const swiper2 = new Swiper('.swiper-gallery-bottom', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper3 = new Swiper('.swiper-gallery', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.gallery-next',
+    prevEl: '.gallery-prev',
+  },
+  thumbs: {
+    swiper: swiper2,
+  },
+});
+
+const swiper4 = new Swiper('.building-swiper', {
+  // centeredSlides: true,
+  // spaceBetween: 40,
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.pagination-building',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.button-building-next',
+    prevEl: '.button-building-prev',
+  },
+});
+
+const swiper5 = new Swiper('.swiper-text', {
+  loop: true,
+  // spaceBetween: 10,
+  slidesPerView: 1,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper6 = new Swiper('.swiper-img', {
+  loop: true,
+  // spaceBetween: 10,
+  navigation: {
+    nextEl: '.button-choose-next',
+    prevEl: '.button-choose-prev',
+  },
+  pagination: {
+    el: '.pagination-choose',
+    type: 'fraction',
+  },
+  thumbs: {
+    swiper: swiper5,
+  },
+});
+
+const eye = document.querySelector('.container-eye');
+const eyeOpen = document.querySelector('.letter-hover');
+
+eye.addEventListener('mouseenter', () => {
+  eyeOpen.style.opacity = '1';
+});
+eye.addEventListener('mouseleave', () => {
+  eyeOpen.style.opacity = '0';
+});
+
+// const path = document.querySelector('.some path');
+// const defaultMorhp = document.querySelector('.some path').getAttribute('d');
+// window.addEventListener('click', () => {
+//   gsap.fromTo(
+//     '.some path',
+//     {
+//       scale: 1,
+//     },
+//     {
+//       scale: 4.5,
+//     },
+//   );
+// });
+
+const path = document.querySelector('.js-choose-hover');
+const defaultMorhp = document.querySelector('.some path').getAttribute('d');
+path.addEventListener('mouseenter', () => {
+  gsap.fromTo(
+    '.some path',
+    {
+      scale: 1,
+    },
+    {
+      scale: 4.5,
+    },
+  );
+});
+path.addEventListener('mouseleave', () => {
+  gsap.fromTo(
+    '.some path',
+    {
+      scale: 4.5,
+    },
+    {
+      scale: 1,
+    },
+  );
+});
